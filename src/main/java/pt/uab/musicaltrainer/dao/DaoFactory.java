4f87195c0ab0a0d32dca8b389150e3a6c2002d53
Daniel Junior <jrzabott@gmail.com>
2026-04-26 12:46:23 +0100

    refactor: adicionar logging a DaoFactory com entradas e saídas de criação de DAOs

--- src/main/java/pt/uab/musicaltrainer/dao/DaoFactory.java
+++ src/main/java/pt/uab/musicaltrainer/dao/DaoFactory.java
@@ -1,6 +1,8 @@
 package pt.uab.musicaltrainer.dao;
 
 import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,21 +11,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class DaoFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(DaoFactory.class);
     private final DataSource dataSource;
 
     public DaoFactory(DataSource dataSource) {
         this.dataSource = dataSource;
+        logger.info("DaoFactory inicializado com DataSource");
     }
 
     public SessionDao createSessionDao() {
-        return new SessionDao(dataSource);
+        logger.debug("Criando instância de SessionDao");
+        SessionDao dao = new SessionDao(dataSource);
+        logger.debug("SessionDao criada com sucesso");
+        return dao;
     }
 
     public ExerciseDao createExerciseDao() {
-        return new ExerciseDao(dataSource);
+        logger.debug("Criando instância de ExerciseDao");
+        ExerciseDao dao = new ExerciseDao(dataSource);
+        logger.debug("ExerciseDao criada com sucesso");
+        return dao;
     }
 
     public ResultDao createResultDao() {
-        return new ResultDao(dataSource);
+        logger.debug("Criando instância de ResultDao");
+        ResultDao dao = new ResultDao(dataSource);
+        logger.debug("ResultDao criada com sucesso");
+        return dao;
     }
 }
